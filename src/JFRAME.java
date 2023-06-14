4b11d5bb777a9a911510a5e6c7fefb969bdd2a5f
Gatanna501st <ggatanna@gmail.com>
2023-06-14 13:43:26 +0300

    Initial commit

---
+++ src/JFRAME.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class JFRAME {
+public static void main(String[]args){
+    JFrame frame = new JFrame();
+    frame.setTitle ("frame goes here");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.setSize(420,420);
+    frame.setVisible(true);
+
+    JPanel panel = new JPanel();
+     
+}
+}
